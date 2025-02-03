26dbef3a90eb12769a06ebcf86118d13c0b9e5b2
schechtr <schechter@g.ucla.edu>
2025-02-03 00:22:22 -0800

    use constants for the game width and height

--- thegamecalledsex.com/src/main.ts
+++ thegamecalledsex.com/src/main.ts
@@ -1,10 +1,11 @@
 import Phaser from "phaser";
 import Game from "./scenes/game";
+import { GAME_WIDTH, GAME_HEIGHT } from "./constants";
 
 const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    width: 390,
-    height: 180,
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT,
     scene: [Game],
     physics: {
         default: "arcade",
