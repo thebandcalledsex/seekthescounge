bd88c450d755e3d54fba37d548f9cd878a362be7
Andrew <schechter@g.ucla.edu>
2025-03-17 18:14:15 -0700

    make increment version script more robust

--- thegamecalledsex.com/utilities/increment_version.sh
+++ thegamecalledsex.com/utilities/increment_version.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Get current version number from src/constants.ts
-current_version=$(grep 'GAME_VERSION' src/constants.ts | awk -F'"' '{print $2}')
+current_version=$(grep 'GAME_VERSION' $SCRIPT_DIR/../src/constants.ts | awk -F'"' '{print $2}')
 
 # Calculate new version
 new_version=$(echo "$current_version + 0.01" | bc | awk '{printf "%.2f", $0}')
 
 # Replace version in src/constants.ts
-sed -i "s/GAME_VERSION = \"$current_version\"/GAME_VERSION = \"$new_version\"/" src/constants.ts
+sed -i '' "s|GAME_VERSION = \"$current_version\"|GAME_VERSION = \"$new_version\"|" $SCRIPT_DIR/../src/constants.ts
 
 
