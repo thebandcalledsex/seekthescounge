8af7726d8ebb0189eecdd4e3058e7aa597b7a7fb
Andrew <schechter@g.ucla.edu>
2025-09-12 00:57:05 -0700

    reorganize assets; add shuey run, desert tiles

---
+++ thegamecalledsex.com/assets/tilesets/desert-tiles.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="desert-tiles" tilewidth="16" tileheight="16" tilecount="45" columns="9">
+ <image source="desert.png" width="144" height="80"/>
+ <tile id="1">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
