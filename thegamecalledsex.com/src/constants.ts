2cea3c8ddf7dd250a65a61b42ab144b663abe98f
Andrew <schechter@g.ucla.edu>
2025-10-06 22:49:21 -0700

    add some snappiness constants

--- thegamecalledsex.com/src/constants.ts
+++ thegamecalledsex.com/src/constants.ts
@@ -1,4 +1,14 @@
-export const GAME_WIDTH = 390;
-export const GAME_HEIGHT = 180;
+export const GAME_WIDTH = 240;
+export const GAME_HEIGHT = 135;
 
-export const GAME_VERSION = "0.4";
+// camera follow snappiness
+export const CAMERA_FOLLOW_LERP_X = 0.1;
+export const CAMERA_FOLLOW_LERP_Y = 0.1;
+export const CAMERA_FOLLOW_OFFSET_X = -40;
+export const CAMERA_FOLLOW_OFFSET_Y = 0;
+
+export const GAME_VERSION = "0.60";
+
+// dialog box snap shut/snap open snappiness
+export const DIALOG_SNAP_OPEN_DURATION = 400; // ms
+export const DIALOG_SNAP_CLOSE_DURATION = 200; // ms
