56b3c94a552cffbacd8a02f0f1c4c6a922d7db3a
Andrew <schechter@g.ucla.edu>
2025-10-07 18:39:03 -0700

    add quick controls for dialog boxes

--- seekthescounge.org/src/constants.ts
+++ seekthescounge.org/src/constants.ts
@@ -7,8 +7,14 @@ export const CAMERA_FOLLOW_LERP_Y = 0.1;
 export const CAMERA_FOLLOW_OFFSET_X = -40;
 export const CAMERA_FOLLOW_OFFSET_Y = 0;
 
-export const GAME_VERSION = "0.61";
+export const GAME_VERSION = "0.70";
 
 // dialog box snap shut/snap open snappiness
-export const DIALOG_SNAP_OPEN_DURATION = 400; // ms
-export const DIALOG_SNAP_CLOSE_DURATION = 200; // ms
+export const DIALOG_SNAP_OPEN_DURATION = 600; // ms
+export const DIALOG_SNAP_CLOSE_DURATION = 400; // ms
+
+// dialog box font size
+export const DIALOG_FONT_SIZE = "10px";
+
+// dialog box char width guess for paging math
+export const DIALOG_CHAR_WIDTH_GUESS = 6;
